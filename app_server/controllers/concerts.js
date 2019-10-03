5a2db8d548a6f3e84cc6ce2e07a4d826175a7a1d
Darragh O Brien <Darragh.OBrien2@students.ittralee.ie>
2019-10-03 13:56:11 +0100

    ListConcertsMore

--- app_server/controllers/concerts.js
+++ app_server/controllers/concerts.js
@@ -23,7 +23,16 @@ entertainment: '4',
 production: '4',
 vocals: '4',
 value: '3',
-comment: 'Was such a good show! The production team did a fantastic job of making the stage look spectacular. Played for around 2 hours and was hoping for more'
+comment: '"Was such a good show! The production team did a fantastic job of making the stage look spectacular. Played for around 2 hours and was hoping for more"'
+}, {
+artistName: 'Ariana Grande',
+venueName: '3Arena',
+entertainment: '5',
+production: '5',
+vocals: '5',
+value: '3',
+comment: '"Ariana was AMAZINGGGG!!!! EVERYTHING WAS SO PERFECT! One bad thing I would say is that the tickets were so overpriced but i had to see her when she was in Ireland"'
+
 }]
 });
 };
