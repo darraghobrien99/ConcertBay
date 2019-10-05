10aa114d966c250ba485c64fb95cef3e98909fb3
darraghobrien99 <43144698+darraghobrien99@users.noreply.github.com>
2019-10-05 15:08:14 +0100

    MongoDB work

---
+++ app_server/models/concerts.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const concertSchema = new mongoose.Schema({
+artistName: {
+type: String,
+required: true
+},
+
+venueName: {
+type: String,
+required: true
+},
+
+entertainment: {
+type: Number,
+'default': 0,
+min: 0,
+max: 5
+
+},
+
+production: {
+type: Number,
+'default': 0,
+min: 0,
+max: 5
+
+},
+
+vocals: {
+type: Number,
+'default': 0,
+min: 0,
+max: 5
+
+},
+
+value: {
+type: Number,
+'default': 0,
+min: 0,
+max: 5
+
+},
+
+comment: {
+type: String,
+required: true
+},
+
+createdOn: {
+type: Date,
+'default': Date.now
+}
+
+ });
+
+mongoose.model('Concert', concertSchema);
