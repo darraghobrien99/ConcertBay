8c1ab8907c47be07ac9dd326b07a5be16a714963
darraghobrien99 <43144698+darraghobrien99@users.noreply.github.com>
2019-10-21 20:33:32 +0100

    User schema, account controller, api work

--- app_api/controllers/concerts.js
+++ app_api/controllers/concerts.js
@@ -6,14 +6,39 @@ const concertsCreate = function (req, res) {
 res
 .status(200)
 .json({"status" : "success"});
+
  };
 const concertsReadOne = function (req, res){
-
-res 
-.status(200) 
-.json({"status" : "success"}); 
-
+  if (req.params && req.params.reviewid) {
+    Con
+      .findById(req.params.reviewid)
+      .exec((err, concert) => {
+        if (!concert) {
+          res	
+            .status(404) 
+            .json({	
+              "message": "ReviewID not found"
+            });	 
+          return;
+        } else if (err) {
+          res	
+            .status(404) 
+            .json(err); 
+          return; 	
+        }
+        res		
+          .status(200)
+          .json(concert);
+      });
+  } else {		
+    res		
+      .status(404) 	
+      .json({	
+        "message": "No ReviewID in request"
+      });		
+  }
 };
+
 
 const concertsUpdateOne = function (req, res) {
 res
--- app_server/controllers/account.js
+++ app_server/controllers/account.js
@@ -5,8 +5,27 @@ res.render('login', { title: 'Login' });
 };
 
 const signup = function(req, res, next){ 
-res.render('signup', { title: 'Sign up' }); 
+res.render('signup', { 
+	title: 'Sign Up',
+	pageHeader: {
+		title: 'Welcome to ConcertBay'
+			},
+users: [{ 
+fullName: 'Darragh OBrien',
+email: 'darraghobrien@gmail.com',
+username: 'darragh257',
+password: 'darragh101'
+}, {
+fullName: 'John OShea',
+email: 'johnoshea@gmail.com',
+username: 'johnoshea',
+password: 'johnny2019'
+
+}]
+});
 };
+
+
 module.exports = { 
 login,
 signup
